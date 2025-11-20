31f87ad7b4c1b8fc0bf5373b3acf25974553a806
C-Hajduk <christian.hajduk@outlook.de>
2025-11-20 09:44:58 +0100

    add button border & underline; add Burgermenu icon

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [NgClass],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  openMenu: boolean = false;
 
+  activeButton: 'btn1' | 'btn2' = 'btn1';
+
+  setActive(button: 'btn1' | 'btn2') {
+    this.activeButton = button;
+  }
+
+  startAnimation() {
+    this.openMenu = !this.openMenu;
+    // if(this.openMenu){
+    //   this.openMenu = false;
+    // }
+    // else{
+    //   this.openMenu = true;
+    // }
+  }
 }
