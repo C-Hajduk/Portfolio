f195ce70a84264f02136ef18c3793ccdc986336c
C-Hajduk <christian.hajduk@outlook.de>
2025-11-24 11:58:42 +0100

    add legal notice, add links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainContent } from './main-content/main-content';
+import { LegalNotice } from './imprint/legal-notice/legal-notice';
 
 export const routes: Routes = [
-    {path: '', component: MainContent}
+    {path: '', component: MainContent},
+    {path: 'legalnotice', component: LegalNotice}
 ];
--- src/app/imprint/imprint.ts
+++ src/app/imprint/imprint.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { LegalNotice } from "./legal-notice/legal-notice";
+import { Header } from "../shared/header/header";
+import { Footer } from "../shared/footer/footer";
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  imports: [LegalNotice, Header, Footer],
   templateUrl: './imprint.html',
   styleUrl: './imprint.scss',
 })
