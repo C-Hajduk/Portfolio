96068a70eae8ceecf4385bc798c0b5ea62a894d6
C-Hajduk <christian.hajduk@outlook.de>
2025-11-28 11:20:39 +0100

    feat: chnage ngIf with event

--- src/app/imprint/legal-notice/legal-notice.ts
+++ src/app/imprint/legal-notice/legal-notice.ts
@@ -1,14 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [TranslatePipe, NgIf],
+  imports: [TranslatePipe],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
 export class LegalNotice {
   translate = inject(TranslateService);
+
+  language = this.translate.currentLang || 'en';
+
+  constructor() {
+    this.translate.onLangChange.subscribe((event) => {
+      this.language = event.lang;
+    });
+  }
 }
