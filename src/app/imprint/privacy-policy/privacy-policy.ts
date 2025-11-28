3337213dcac05e992a792c97ebc14763fb9f10d9
C-Hajduk <christian.hajduk@outlook.de>
2025-11-28 17:27:38 +0100

    feat: add privacy policy

--- src/app/imprint/privacy-policy/privacy-policy.ts
+++ src/app/imprint/privacy-policy/privacy-policy.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  standalone: true,
+  imports: [TranslatePipe],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
 })
 export class PrivacyPolicy {
+  translate = inject(TranslateService);
 
+  language = this.translate.currentLang || 'en';
+
+  constructor() {
+    this.translate.onLangChange.subscribe((event) => {
+      this.language = event.lang;
+    });
+  }
 }
