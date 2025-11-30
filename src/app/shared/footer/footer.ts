27baca1df0467e27f555874306fbfe11ce35b5de
C-Hajduk <christian.hajduk@outlook.de>
2025-11-30 18:22:01 +0100

    feat: add Routerlink

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { BurgermenuService } from '../../burgermenu-service';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
